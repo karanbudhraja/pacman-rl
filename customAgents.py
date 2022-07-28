f12b3a2c05f771e4ede968bd0602d2fa22938bf5
karan <karanb1@umbc.edu>
2022-07-27 22:55:25 -0500

    template for custom agent

---
+++ customAgents.py
@@ -0,0 +1,18 @@
+#
+# custom agents added here
+#
+
+from game import Agent
+from game import Directions
+import random
+
+class CustomAgent(Agent):
+    """
+    A custom agent.
+    """
+    def getAction( self, state):
+        legal = state.getLegalActions(self.index)
+        move = Directions.STOP
+        move = random.choice(legal)
+
+        return move
