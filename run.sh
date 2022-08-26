24c7aa11480d06595c7d063c4db2072a52f07664
karan <karanb1@umbc.edu>
2022-08-25 20:10:27 -0500

    wip: setup value learning

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ AGENT_TYPE="CustomAgent"
 GHOST_TYPE="DirectionalGhost"
 LAYOUT_TYPE="smallClassic"
 NUMBER_OF_RUNS="10000"
-python3 pacman.py -p $AGENT_TYPE -g $GHOST_TYPE -l $LAYOUT_TYPE -n $NUMBER_OF_RUNS
+python3 pacman.py -p $AGENT_TYPE -g $GHOST_TYPE -l $LAYOUT_TYPE -n $NUMBER_OF_RUNS -q
